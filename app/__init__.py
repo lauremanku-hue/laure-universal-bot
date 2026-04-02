1950abb963e826276e72c58981a77c0c0503add9
data scientist <laure.manku@facsciences-uy1.cm>
2026-04-02 18:52:59 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from .extensions import db
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///laure_session.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/laure_session.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
